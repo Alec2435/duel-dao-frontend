081e79ca9c9c881b26bd8cdf64893a62afd56c55
Andre Terron <git@terron.me>
2022-02-14 12:29:14 -0700

    Remove logs and comments

--- apps/web/pages/app/game/[gameid].tsx
+++ apps/web/pages/app/game/[gameid].tsx
@@ -3,9 +3,7 @@ import { useChessBoardState } from "../../../src/service/contract-interface"
 
 const GamePage = (props) => {
     const router = useRouter()
-    console.log("Query", router.query)
     const gameId = router.query.gameid as string
-    console.log("Game ID", gameId)
     const [boardState, refreshBoardState] = useChessBoardState(gameId)
 
     return <pre>{JSON.stringify(boardState, null, 4)}</pre>
--- apps/web/src/service/contract-interface.ts
+++ apps/web/src/service/contract-interface.ts
@@ -7,7 +7,7 @@ const contractAddress = '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318';
 const contractABI = abi;
 
 export type RawGameState = number[]
-export type GameID = string // TODO: byte32
+export type GameID = string
 export type Web3Provider = ethers.providers.ExternalProvider | ethers.providers.JsonRpcFetchFunc
 
 export async function initGame(provider: ethers.providers.Web3Provider, player1Alias: string) {
@@ -16,13 +16,7 @@ export async function initGame(provider: ethers.providers.Web3Provider, player1A
 
     const initTxn = await chessContract.initGame(player1Alias, true, 3600);
 
-    // const createTxn = await keyboardsContract.create(keyboardKind, isPBT, filter)
-    // console.log('Create transaction started...', createTxn.hash)
-    console.log("initTxn", initTxn)
-
     const waited = await initTxn.wait();
-
-    console.log("waited", waited)
 
     return initTxn.hash
 }
@@ -33,7 +27,6 @@ export async function getPlayerGames(provider: ethers.providers.Web3Provider, ad
     const chessContract = new ethers.Contract(contractAddress, contractABI, signer);
 
     const gameIds = await chessContract.getGamesOfPlayer(address);
-    console.log("Game IDs", gameIds)
 
     return gameIds
 }
@@ -69,7 +62,6 @@ export async function getChessBoardState(provider: ethers.providers.Web3Provider
     const chessContract = new ethers.Contract(contractAddress, contractABI, signer);
 
     const gameState = await chessContract.getCurrentGameState(gameId);
-    console.log("Game State", gameState)
 
     return gameState
 }
@@ -79,15 +71,12 @@ export function useChessBoardState(gameId: GameID): [any, () => void] {
     const [boardState, setBoardState] = useState(null)
 
     async function refreshBoardState() {
-        console.log("Refreshing board", {gameId, address, provider})
         if (provider && address && gameId) {
-            console.log("Refreshing board FOREAL", gameId, address)
             const state = await getChessBoardState(provider, gameId)
             setBoardState(state)
         } else if (boardState !== null) {
             setBoardState(null)
         }
-        console.log("Done Refreshing")
     }
 
     // Refreshes the data on first run, and if the gameId changes
@@ -103,59 +92,3 @@ export function useChessBoardState(gameId: GameID): [any, () => void] {
         boardState, refreshBoardState
     ]
 }
-
-/*
-
-import { useEffect, useState } from "react"
-import { abi } from "./contract-abi/Chess.json"
-import { ethers } from "ethers"
-
-// const contractAddress = '0xe6748c2E34dF3A435fbCa90199f9F153Fa9F4Cde';
-// const contractABI = abi;
-
-/**
- * 
- * @param {number} gameId byte32
- * @returns 
- 
- export async function getChessBoardState(gameId) {
-    // TODO: need to get the ethereum
-    // if (ethereum && connectedAccount) {
-    //     const provider = new ethers.providers.Web3Provider({});
-    //     const signer = provider.getSigner();
-    //     const chessContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-    //     const currentGameState = await chessContract.getCurrentGameState(gameId);
-    // }
-    // return currentGameState
-    return {}
-}
-
-/**
- * 
- * @param {number} gameId byte32
- * @returns {[null | {}, () => void]}
- 
-export function useChessBoardState(gameId) {
-    const [boardState, setBoardState] = useState(null)
-
-    async function refreshBoardState() {
-        const state = await getChessBoardState(gameId)
-        setBoardState(state)
-    }
-
-    // Refreshes the data on first run, and if the gameId changes
-    useEffect(() => {
-        // If the gameId changed, and we have a boardState, change it to null while we load the new one
-        if (boardState !== null) {
-            setBoardState(null)   
-        }
-        refreshBoardState()
-    }, [gameId])
-
-    return [
-        boardState, refreshBoardState
-    ]
-}
-
-*/
